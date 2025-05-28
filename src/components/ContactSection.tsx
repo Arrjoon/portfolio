5fe64ec4663a1aebc09cd442e5ec8bb8c9b052d3
Arrjoon <nepaliarjun049@gmail.com>
2025-05-28 15:23:58 +0545

    fixed issues

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState } from 'react';
-import { FaPaperPlane } from 'react-icons/fa';
+
 
 interface FormData {
   name: string;
@@ -83,7 +83,7 @@ export default function ContactSection() {
               type="submit"
               className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
             >
-              Send Message <FaPaperPlane className="ml-2" />
+        
             </button>
           </div>
         </form>
